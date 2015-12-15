c649d0372e86484f46bf2d1f632162bf1422e184
Ian <inorris372@gmail.com>
2015-12-15 00:34:34 -0600

    Initial Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Aerospace-Engineering-MATLAB-Exercises
-Contains MATLAB scripts that I wrote for various assignments throughout my college career
--- myOpFun.m
+++
@@ -1,8 +0,0 @@
-function f = myOpFun(x)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-f = pi^2*x(1)^4+pi^2*x(1)^2*x(2)^2;
-
-
-end
-
